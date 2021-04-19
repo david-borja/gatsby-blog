fa9b10c7bf04573e2b47b7329ed5e80874904e92
dav1dborja <davidborjamartinez@gmail.com>
2021-04-19 17:58:51 +0200

    created dummy blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,58 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { node } from "prop-types"
 
-const IndexPage = () => (
+const BlogLink = styled(Link)`
+  text-decoration: none
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <div>
+      <h1>David's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {
+        data.allMarkdownRemark.edges.map(({node}) => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>{node.frontmatter.title} - {node.frontmatter.date}</BlogTitle>
+            </BlogLink>
+              <p>{node.excerpt}</p>
+          </div>
+        ))
+      }
+    </div>
   </Layout>
 )
 
-export default IndexPage
+export const query = graphql`
+  query MyQuery {
+  allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+    totalCount
+    edges {
+      node {
+        id
+        frontmatter {
+          date
+          title
+        }
+        fields {
+          slug
+        }
+        excerpt
+      }
+    }
+  }
+}
+`
